034a66bdb8e0e5cc196c04fd214196432b1ff714
Chase Lambert <theicfire@gmail.com>
2015-03-16 23:57:41 -0700

    Have endpoint that wraps pushbullet

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 import os
-from flask import Flask
+from pushbullet import Pushbullet
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+pb = Pushbullet('oYHlSULc3i998hvbuVtsjlH0ps23l7y2')
 
 @app.route('/')
 def hello():
-        return 'python/flask'
+    return 'python/flask'
+
+@app.route('/send', methods=['POST'])
+def send():
+    return jsonify(**pb.push_note(request.form['title'],request.form['message']))
+    #return 'done!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
